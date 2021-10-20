7e626366162776738e6f6982d00d6d9b9ee65a74
kashcodernyc <prakash_subba08@hotmail.com>
2021-10-20 12:03:28 -0400

    first-commit

---
+++ src/data.js
@@ -0,0 +1,9 @@
+export default [
+    {
+        id: 1,
+        name: 'The Weather App',
+        date: 'June 2021',
+        image: './images/forest.jpeg',
+
+    },
+]
--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDom from 'react-dom';
+import Navbar from './navbar';
+import About from './about';
+import Portfolio from './portfolio';
+import ScrollToTop from './scroll';
+import { Route, BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function App(){
+  return( 
+  <Router>
+    <ScrollToTop/>
+  <section className = 'mainpage'>
+    <div className = 'container'>
+      <Navbar/>
+      <div className = 'maincontent'>
+        <p>Prakash is a New York City based web designer/developer.</p>
+      </div>
+    </div>
+    <section className = 'aboutme' id = 'about'>
+    <About/>
+    </section>
+    <Portfolio/>
+  
+  </section>
+  </Router>
+  
+
+)
+
+}
+
+ReactDom.render(<App/>, document.getElementById('root'));
